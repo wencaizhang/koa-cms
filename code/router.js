36411e81999dd62ece9cccbdbc664ad1b2aa138a
zhangwencai <zhangwencai@urplus.cn>
2019-02-26 15:29:39 +0800

    create advise page

--- code/router.js
+++ code/router.js
@@ -1,16 +1,17 @@
-const router = require('koa-router')()
+const router = require("koa-router")();
 
-module.exports = (app) => {
-  router.get( '/', app.controller.home.index )
-  
-  router.get('/home', app.controller.home.home)
-  
-  router.get('/home/:id/:name', app.controller.home.homeParams)
-  
-  router.get('/user', app.controller.home.login)
-  
-  router.post('/user/register', app.controller.home.register)
-  
-  app.use(router.routes())
-     .use(router.allowedMethods())
-}
+module.exports = app => {
+  router.get("/", app.controller.home.index);
+
+  router.get("/home", app.controller.home.home);
+
+  router.get("/home/:id/:name", app.controller.home.homeParams);
+
+  router.get("/user", app.controller.home.login);
+
+  router.post("/user/register", app.controller.home.register);
+
+  router.get("/advise", app.controller.advise.index);
+
+  app.use(router.routes()).use(router.allowedMethods());
+};
